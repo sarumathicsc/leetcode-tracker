aa0e21beb301d82b231fe31275d507a5303441ab
sarumathicsc <165637512+sarumathicsc@users.noreply.github.com>
2026-07-09 07:41:23 +0530

    Create 1569-MaxDotProductOfTwoSubsequences

---
+++ 1569-MaxDotProductOfTwoSubsequences/1569-MaxDotProductOfTwoSubsequences.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 7:41:23 AM
+class Solution {
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = Integer.MIN_VALUE / 2; 
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                int product = nums1[i - 1] * nums2[j - 1];
+                dp[i][j] = Math.max(
+                    product,
+                    Math.max(
+                        product + Math.max(0, dp[i - 1][j - 1]),
+                        Math.max(dp[i - 1][j], dp[i][j - 1])    
+                    )
+                );
+            }
+        }
+
+        return dp[m][n];
+    }
+}
