30c6c2f5045bd81b4e9800de51e38dcba72c4ef0
sarumathicsc <165637512+sarumathicsc@users.noreply.github.com>
2026-07-09 07:39:53 +0530

    Create 4318-ValidSubarraysWithMatchingSumDigitsI

---
+++ 4318-ValidSubarraysWithMatchingSumDigitsI/4318-ValidSubarraysWithMatchingSumDigitsI.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 7:39:53 AM
+class Solution {
+    public int countValidSubarrays(int[] nums, int x) {
+        int n = nums.length;
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            long sum = 0;
+
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+                long temp = sum;
+                int lastDigit = (int)(temp%10);
+
+                while(temp >= 10){
+                    temp /= 10;
+                }
+                int firstDigit = (int) temp;
+
+                if(firstDigit == x && lastDigit == x){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
