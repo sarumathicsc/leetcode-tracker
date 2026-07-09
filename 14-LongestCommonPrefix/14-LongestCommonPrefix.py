747a54011ea5821684974b5068eac534d9e2e6b0
sarumathicsc <165637512+sarumathicsc@users.noreply.github.com>
2026-07-09 07:42:51 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,16 @@
+# Last updated: 7/9/2026, 7:42:51 AM
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        
+        prefix = strs[0]
+        
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
+        
