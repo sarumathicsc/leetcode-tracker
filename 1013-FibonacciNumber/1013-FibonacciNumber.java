ade3632b9011fbc986fdcda8499f90224898c390
sarumathicsc <165637512+sarumathicsc@users.noreply.github.com>
2026-07-09 07:41:49 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 7:41:49 AM
+class Solution {
+    public int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    
+    }
+}
