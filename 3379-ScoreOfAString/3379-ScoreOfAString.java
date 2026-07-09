49a74c9a3d4ff7f9eb94b57dfab2e4a4d2f24417
sarumathicsc <165637512+sarumathicsc@users.noreply.github.com>
2026-07-09 07:40:22 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 7:40:23 AM
+class Solution {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for(int i = 0; i < s.length()-1; i++){
+            sum += Math.abs(s.charAt(i)-s.charAt(i+1));
+        }
+        return sum;
+    }
+}
